468dd71cb65f84b0323eee9d39c4fe8d12c5811d
simonjoe246 <simonjoe246@gmail.com>
2023-02-19 21:46:03 +0800

    Site updated: 2023-02-19 21:46:03

---
+++ scripts/baiduanalytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1e68cb7dee7f491ede637878bfb55e22";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
